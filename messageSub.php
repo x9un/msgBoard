c8f60520f6435e03e934f546cd08290805190206
357 <59087822+x9un@users.noreply.github.com>
2020-04-03 20:18:14 +0800

    Update messageSub.php

--- messageSub.php
+++ messageSub.php
@@ -8,6 +8,7 @@ $dir="img";
 $is_upload = false;
 $msg = null;
 $img_path=null;
+$temp_path=null;
 
 if($_FILES['file']['name']>=88){
     echo "<script>alert('文件上传失败');history.go(-1)</script>";
@@ -16,33 +17,49 @@ if($_FILES['file']['name']>=88){
 
 if(!empty($_FILES['file']['size'])&&($_FILES['file']['size'])<1048577){
     //检查MIME
-    $allow_type = array('image/jpeg','image/png','image/gif');
+    $allow_type = array('image/jpeg','image/png');
     if(!in_array($_FILES['file']['type'],$allow_type)){
         echo "<script>alert('禁止上传该类型文件');history.go(-1)</script>";
     }else{
         //检查文件名
         $file =  $_FILES['file']['name'];
        
-            $file = explode('.', strtolower($file));
+        $file = explode('.', strtolower($file));
        
 
         $ext = end($file);
-        $allow_suffix = array('jpg','png','gif');
+        $allow_suffix = array('jpg','png','jpeg');
         if (!in_array($ext, $allow_suffix)) {
             echo "<script>alert('禁止上传该后缀文件');history.go(-1)</script>";
         }else{
             $file_name = reset($file) . '.' . $file[count($file) - 1];
             $temp_file = $_FILES['file']['tmp_name'];
+            $temp_path=$_FILES['file']['tmp_name'];
             $img_path = $dir . '/' .$file_name;
 
-            $size=getimagesize($img_path);
+
+            if( $_FILES['file']['type'] == 'image/jpeg' ) {
+                $img = imagecreatefromjpeg( $temp_file );
+                imagejpeg( $img, $temp_path, 100);
+            }
+            else if($_FILES['file']['type'] == 'image/png') {
+                $img = imagecreatefrompng( $temp_file );
+                imagepng( $img, $temp_path);
+            }
+
+            imagedestroy( $img );
+
+            $size=getimagesize($temp_path);
             $width=$size[0];
             $height=$size[1];
-echo "1";
+
+
+
+
             if($width>1000||$height>1000){
                 echo "<script>alert('文件上传失败');history.go(-1)</script>";
                 exit();
-            }else if (move_uploaded_file($temp_file, $img_path)) {
+            }else if (move_uploaded_file($temp_path, $img_path)) {
      
                 echo "<script>alert('文件上传成功');history.go(-1)</script>";
                 $is_upload = true;
@@ -84,7 +101,6 @@ try{
 $dsn="mysql:host=$mysrv;dbname=$mydb;";
 $pdo=new PDO($dsn,$myuser,$mypwd);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$pdo->setAttribute(PDO::MYSQL_ATTR_MULTI_STATEMENTS, false);
 if ($is_upload){
   $sql = $pdo->prepare("insert into comment (username,message,photo,pub_date) values (?,?,?,?)");
   $sql->execute(array($username,$message,$img_path,$pub_date));
