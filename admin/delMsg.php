5c7c52f6de1ea4d191cad303ceee2fcafd9ecfe4
357 <59087822+x9un@users.noreply.github.com>
2020-04-05 19:53:51 +0800

    Update delMsg.php

--- admin/delMsg.php
+++ admin/delMsg.php
@@ -7,7 +7,8 @@ if (isset($_SESSION['admin']) && !empty($_GET['id'])) {
 	try{
             $dsn="mysql:host=$mysrv;dbname=$mydb;";
             $pdo=new PDO($dsn,$myuser,$mypwd);
-
+	    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $pdo->setAttribute(PDO::MYSQL_ATTR_MULTI_STATEMENTS, false);
 	        $sql=$pdo->prepare("DELETE FROM comment WHERE id = ? LIMIT 1");
             $res=$sql->execute(array($id));
             if($res){
